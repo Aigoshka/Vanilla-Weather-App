2f76fd05cf0d1de0b36fc56f68e8cb0ea52a45be
Aigoshka <74876549+Aigoshka@users.noreply.github.com>
2020-12-08 22:08:36 +0100

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+function formatHour (timestamp){
+  let date = new Date (timestamp);
+    let hour = date.getHours();
+  let minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  return `${hour}:${minute}`
+}
+function formatDate(timestamp){
+    let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  let days = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];  
+  let date = new Date (timestamp);
+  let month=months[date.getMonth()];
+  let day= days[date.getDay()];
+  let currentDate=date.getDate();
+formatDate=`${day}, ${currentDate} ${month}`;
+return formatDate;
+  
+}
+
+
+
 function displayTemperature(response){
     console.log (response.data);
 let temperatureElement =document.querySelector("#current-temp");
@@ -7,7 +40,8 @@ let humidityElement =document.querySelector("#humidity");
 let windElement =document.querySelector("#wind");
 let pressureElement =document.querySelector("#pressure");
 let feelsElement =document.querySelector("#feels-like-deegre");
-
+let dateElement=document.querySelector("#date");
+let hourElement=document.querySelector("#current-hour");
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML=response.data.weather[0].description;
@@ -15,6 +49,8 @@ humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=response.data.wind.speed;
 pressureElement.innerHTML=Math.round(response.data.main.temp_max);
 feelsElement.innerHTML=Math.round(response.data.main.feels_like);
+dateElement.innerHTML=formatDate (response.data.dt*1000);
+hourElement.innerHTML=formatHour (response.data.dt*1000);
 }
 
 
