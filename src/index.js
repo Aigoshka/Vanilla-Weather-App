2c87739e650499fe276d2d67d7e67e0418ce9ced
Aigoshka <74876549+Aigoshka@users.noreply.github.com>
2020-12-10 23:38:12 +0100

    Implemented unit conversion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 let now = new Date ();
-function formatHour (date){
-let hour = (date.getHours() < 10 ? "0" : "") + date.getHours();
-let minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
-formatHour =`${hour}:${minute}`;
-return formatHour;
+function formatHour(date) {
+  let hour = (date.getHours() < 10 ? "0" : "") + date.getHours();
+  let minute = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  return `${hour}:${minute}`;
 }
-function formatDate(timestamp){
-    let months = [
+function formatDate(timestamp) {
+  let months = [
     "Jan",
     "Feb",
     "Mar",
@@ -20,17 +19,24 @@ function formatDate(timestamp){
     "Nov",
     "Dec"
   ];
-  let days = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];  
-  let date = new Date (timestamp);
-  let month=months[date.getMonth()];
-  let day= days[date.getDay()];
-  let currentDate=date.getDate();
-formatDate=`${day}, ${currentDate} ${month}`;
-return formatDate;
-  
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let date = new Date(timestamp);
+  let month = months[date.getMonth()];
+  let day = days[date.getDay()];
+  let currentDate = date.getDate();
+  return `${day}, ${currentDate} ${month}`;
 }
 
 function displayTemperature(response){
+    console.log(response.data);
 let temperatureElement =document.querySelector("#current-temp");
 let cityElement =document.querySelector("#city");
 let descriptionElement =document.querySelector("#current-desc");
@@ -42,6 +48,8 @@ let dateElement=document.querySelector("#date");
 let hourElement=document.querySelector("#current-hour");
 let iconElement=document.querySelector("#icon");
 
+celsiusTemperature=Math.round(response.data.main.temp);
+
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML=response.data.weather[0].description;
@@ -51,7 +59,7 @@ pressureElement.innerHTML=Math.round(response.data.main.temp_max);
 feelsElement.innerHTML=Math.round(response.data.main.feels_like);
 dateElement.innerHTML=formatDate (response.data.dt*1000);
 hourElement.innerHTML=formatHour (now);
-iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
@@ -72,7 +80,7 @@ search("Prague");
 function showPosition(position) {
   let apiKey = "f36e45e370221a0b671266843fbab2eb";
   let apiGeoLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiGeoLocation).then(actualTemperature);
+  axios.get(apiGeoLocation).then(displayTemperature);
 }
 function geoPlace(event) {
   event.preventDefault();
@@ -85,3 +93,26 @@ form.addEventListener("submit", handelSubmit);
 //geoLocation
 let geoButton = document.querySelector("#geo-loc");
 geoButton.addEventListener("click", geoPlace);
+
+//F|C
+function showTemperatureC(event) {
+  event.preventDefault();
+  let currentTempElement=document.querySelector("#current-temp");
+
+  currentTempElement.innerHTML = celsiusTemperature;
+}
+let temperatureC = document.querySelector("#celsius");
+temperatureC.addEventListener("click", showTemperatureC);
+
+
+let celsiusTemperature=null;
+
+
+function showTemperatureF(event) {
+  event.preventDefault();
+  let currentTempElement=document.querySelector("#current-temp");
+  let fahrenheitTemperature = (celsiusTemperature*9)/5+32;
+currentTempElement.innerHTML=Math.round(fahrenheitTemperature);
+}
+let temperatureF = document.querySelector("#fahrenheit");
+temperatureF.addEventListener("click", showTemperatureF);
